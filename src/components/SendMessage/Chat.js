1d1d4d59d9d75966baceb2d51565a11e84c9660d
alperenkapusuz <sefalperen21@gmail.com>
2022-05-12 20:23:02 +0300

    chat tamamlandi

--- src/components/SendMessage/Chat.js
+++ src/components/SendMessage/Chat.js
@@ -1,14 +1,44 @@
-import React from 'react'
-import { auth } from '../../firebase'
-import {useAuthState} from 'react-firebase-hooks/auth'
+import React, { useState, useEffect } from "react";
+import { auth, db } from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import SendMessage from "./SendMessage";
+import './Chat.css'
 
 function Chat() {
   const [user] = useAuthState(auth);
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    db.collection("messages")
+      .orderBy("createdAt")
+      .limit(50)
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => doc.data()));
+      });
+  }, []);
   return (
     <div>
-      {user ? <div>chat</div> : <div>Giris yap</div>}
+      {user ? (
+        <div className="msgs">
+          {messages.map(({ id, text, photoURL, uid }) => (
+            <div>
+              <div
+                key={id}
+                className={`msg ${
+                  uid === auth.currentUser.uid ? "sent" : "received"
+                }`}
+              >
+                <img src={photoURL} alt="" />
+                <p>{text}</p>
+              </div>
+            </div>
+          ))}
+          <SendMessage/>
+        </div>
+      ) : (
+        <div>Giris yap</div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
---
+++ src/components/SendMessage/SendMessage.js
@@ -0,0 +1,32 @@
+import React,{useState} from 'react'
+import {db, auth} from '../../firebase'
+import firebase from 'firebase/compat/app';
+import { Input, Button } from 'reactstrap';
+import './SendMessage.css'
+
+function SendMessage() {
+    const [msg, setMsg] = useState('')
+
+    async function sendMessage(e){
+        e.preventDefault()
+        const {uid, photoURL} = auth.currentUser
+
+        await db.collection('messages').add({
+            text: msg,
+            photoURL,
+            uid,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        setMsg('')
+    }
+  return (
+    <div>
+      <form onSubmit={sendMessage}>
+        <Input className='sendMessage__input' value={msg} onChange={(e) => setMsg(e.target.value)} placeholder='Message...'/>
+        <Button className='sendMessage__button' type="submit">Send</Button>
+      </form>  
+    </div>
+  )
+}
+
+export default SendMessage
